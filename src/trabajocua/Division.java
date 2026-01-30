17f097cbdb25bd99621a4f7c7b1ff6af47ab5184
Pc15 <Pc15@CPU15>
2026-01-30 10:22:40 -0600

    codigodivfinal

---
+++ src/trabajocua/Division.java
@@ -0,0 +1,15 @@
+
+package trabajocua;
+
+public class Division {
+    
+      public double operacion(double a, double b, double c) {
+
+        if (b == 0 || c == 0) {
+            throw new ArithmeticException("No se puede dividir entre cero");
+        }
+
+        return (a + b) / c;
+    }
+}
+
