ec75fe2528939bda079b931051167018398edc3e
Progra14 <Progra14@Cpu14>
2026-01-31 09:48:19 -0600

    CódigoPrincipalCUA

---
+++ src/trabajocua/TrabajoCUA.java
@@ -0,0 +1,22 @@
+package trabajocua;
+
+import java.util.Scanner;
+
+public class TrabajoCUA {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.println("Ingresa un numero: ");
+        int a = sc.nextInt();
+        System.out.println("Ingresa el sugundo numero: ");
+        int b = sc.nextInt();
+        System.out.println("Ingresa un tercer numero: ");
+        int c = sc.nextInt();
+        //Division division = new Division();
+        //Multiplicacion multiplicacion = new Multiplicacion();
+        //System.out.println("Tu resultado de la division es: "+Division.Operacion(a, b, c));
+        //System.out.println("Tu resultado de la multiplicacion es: "+Multiplicacion.Operacion(a, b, c));
+        
+    }
+    
+}
